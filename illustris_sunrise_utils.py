8822b7835766dfdeecba9165b28c491c48ed8cc3
gsnyder206 <snyder.gregoryf@gmail.com>
2017-03-08 10:14:12 -0500

    add sunrise script

---
+++ illustris_sunrise_utils.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import numpy as np
+import glob
+
+
+#this function takes as inputs the return values of illustris_api_utils.get_subhalo()
+#this is a snapshot file and subhalo metadata object
+
+def setup_sunrise_illustris_subhalo(snap_cutout,subhalo_object,verbose=True,clobber=True):
+
+    snap_dir = os.path.basedir(snap_cutout)
+    print("Setting up sunrise run in.. ", snap_dir)
+
+
+    
+    return
+
