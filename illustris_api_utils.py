010206bef342a2e0458bc46bfea84e6b385f6484
gsnyder206 <gsnyder@stsci.edu>
2017-03-09 09:42:45 -0500

    update hdf5 headers

--- illustris_api_utils.py
+++ illustris_api_utils.py
@@ -46,6 +46,7 @@ start_time = time.time()
 
 defaultparams={'stars':'ParticleIDs','Coordinates,Velocities,GFM_StellarFormationTime,GFM_Metallicity,GFM_InitialMass,Masses','gas':'Coordinates,Density,ElectronAbundance,Masses,Velocities,Volume,SubfindDensity,Potential,InternalEnergy,StarFormationRate,GFM_Metallicity,GFM_AGNRadiation,GFM_WindDMVelDisp,GFM_CoolingRate,NeutralHydrogenAbundance,SmoothingLength,SubfindHsml,SubfindVelDisp,NumTracers,ParticleIDs','dm':'Coordinates,Velocities,Potential','bhs':'all'}
 
+
 baseUrl = 'http://www.illustris-project.org/api/'
 headers = {"api-key":"117782db3bf216d7ce7a04d0c9034601"}
 
