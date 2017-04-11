250819cc0ff00674a1da099e2c2f3b43d2ec28fa
gsnyder206 <snyder.gregoryf@gmail.com>
2017-04-11 10:45:03 -0400

    fix

--- illustris_sunrise_utils.py
+++ illustris_sunrise_utils.py
@@ -34,6 +34,7 @@ def setup_sunrise_lightcone(snap_cutout,subhalo_object,label,this_z,geofile,pos_
 
     run_dir = snap_dir+'/%s'%run_type
 
+    print(subhalo_object)
     sfid=subhalo_object['SubfindID']
         
     print('\tGenerating sunrise.sbatch file for %s...'%run_type)
