175cac9daa6c544e246eec6013a6eadedfdfa49d
gsnyder206 <gsnyder@stsci.edu>
2017-07-03 14:42:04 -0400

    enzo functions

--- illustris_sunrise_utils.py
+++ illustris_sunrise_utils.py
@@ -348,6 +348,106 @@ def setup_sunrise_illustris_subhalo(snap_cutout,subhalo_object,verbose=True,clob
 
 
 
+
+
+def setup_sunrise_enzo(snap_fits,verbose=True,clobber=True,
+                       stub_dir='$HOME/PythonCode/mock-surveys/stubs_illustris/',
+                       data_dir='$HOME/sunrise_data/',
+                       nthreads=24,redshift_override=None,walltime_limit='02:00:00',use_scratch=True):
+
+    fits_file = os.path.abspath(snap_fits)
+    
+    snap_dir = os.path.dirname(fits_file)
+    
+    print("Setting up sunrise run in.. ", snap_dir)
+
+    stub_dir = os.path.expandvars(stub_dir)
+    data_dir = os.path.expandvars(data_dir)
+    
+    print("Using stubs in.. ",stub_dir)
+    stub_files = np.asarray(glob.glob(os.path.join('stub_dir','*')))
+
+    
+    list_of_types = ['images','grism']
+
+    idx=None
+
+    #real_redshift=gsu.redshift_from_snapshot( subhalo_object['snap'] )
+    #scale_convert=(1.0/(gsu.ilh*(1.0 + real_redshift)))
+
+    real_redshift=pyfits.open(snap_fits)['YT'].header['REDSHIFT']
+
+    if redshift_override is None:
+        redshift=real_redshift
+    else:
+        redshift=redshift_override
+
+
+    print('redshift= ',redshift)
+    
+    nthreads=str(nthreads)
+        
+    for run_type in list_of_types:
+        run_dir = snap_dir+'/%s'%run_type
+        if not os.path.lexists(run_dir):
+            os.mkdir(run_dir)
+
+        for sf in stub_files:
+            shutil.copy(sf,run_dir)
+            
+        print('\tGenerating sfrhist.config file for %s...'%run_type)
+        sfrhist_fn   = 'sfrhist.config'
+        sfrhist_stub = os.path.join(stub_dir,'sfrhist_base.stub')
+
+        generate_sfrhist_config(run_dir = run_dir, filename = sfrhist_fn, data_dir=data_dir,
+                                stub_name = sfrhist_stub,  fits_file = fits_file, 
+                                galprops_data = galprops_data, run_type = run_type,
+                                nthreads=nthreads, idx = idx,scale_convert=scale_convert,use_scratch=use_scratch)
+
+
+        print('\tGenerating mcrx.config file for %s...'%run_type)
+        mcrx_fn   = 'mcrx.config'
+        mcrx_stub = os.path.join(stub_dir,'mcrx_base.stub')
+
+        generate_mcrx_config(run_dir = run_dir, snap_dir = snap_dir, filename = mcrx_fn, 
+                             stub_name = mcrx_stub,
+                             galprops_data = galprops_data, run_type = run_type, nthreads=nthreads, cam_file=None , idx = idx,use_scratch=use_scratch)
+
+
+        
+        if run_type == 'images': 
+            print('\tGenerating broadband.config file for %s...'%run_type)
+            broadband_fn   = 'broadband.config'
+            broadband_stub = os.path.join(stub_dir,'broadband_base.stub')
+
+            generate_broadband_config_images(run_dir = run_dir, snap_dir = snap_dir, data_dir=data_dir, filename = broadband_fn, 
+                                             stub_name = broadband_stub, 
+                                             galprops_data = galprops_data, idx = idx,redshift=redshift,use_scratch=use_scratch)
+        if run_type == 'grism': 
+            print('\tGenerating broadband.config file for %s...'%run_type)
+            broadband_fn   = 'broadband.config'
+            broadband_stub = os.path.join(stub_dir, 'broadband_base.stub')
+
+            generate_broadband_config_grism(run_dir = run_dir, snap_dir = snap_dir, data_dir=data_dir, filename = broadband_fn, 
+                                            stub_name = broadband_stub, 
+                                            galprops_data = galprops_data, idx = idx,redshift=redshift,use_scratch=use_scratch)
+
+
+
+
+
+        print('\tGenerating sunrise.sbatch file for %s...'%run_type)
+        sbatch_fn   = 'sunrise.sbatch'		
+        final_fn = generate_sbatch(run_dir = run_dir, snap_dir = snap_dir, filename = sbatch_fn, 
+                                 galprops_data = galprops_data, run_type = run_type,ncpus=nthreads,walltime=walltime_limit,use_scratch=use_scratch)
+
+        
+    
+    return
+
+
+
+
 def generate_sfrhist_config(run_dir, filename, data_dir, stub_name, fits_file, galprops_data, run_type, nthreads='1', idx = None,scale_convert=1.0,use_scratch=False,isnap=None):
     if use_scratch is True:
         if isnap is not None:
