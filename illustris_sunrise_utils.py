5e01bdb6f6ea0ef88cf8a92b94166c1e149d8259
gsnyder206 <snyder.gregoryf@gmail.com>
2018-06-05 10:21:42 -0400

    update for LPS

--- illustris_sunrise_utils.py
+++ illustris_sunrise_utils.py
@@ -256,7 +256,7 @@ def generate_sbatch_lightcone(run_dir, snap_dir, filename, galprops_data, run_ty
 
 def setup_sunrise_illustris_subhalo(snap_cutout,subhalo_object,verbose=True,clobber=True,
                                     stub_dir='$HOME/PythonCode/mock-surveys/stubs_illustris/',
-                                    data_dir='$HOME/sunrise_data/',
+                                    data_dir='$HOME/sunrise_data/',light_filter_set=False,
                                     nthreads=24,redshift_override=None,walltime_limit='02:00:00',use_scratch=True,bhradeff=0.2):
 
     fits_file = os.path.abspath(snap_cutout)
@@ -324,7 +324,7 @@ def setup_sunrise_illustris_subhalo(snap_cutout,subhalo_object,verbose=True,clob
             broadband_stub = os.path.join(stub_dir,'broadband_base.stub')
 
             generate_broadband_config_images(run_dir = run_dir, snap_dir = snap_dir, data_dir=data_dir, filename = broadband_fn, 
-                                             stub_name = broadband_stub, 
+                                             stub_name = broadband_stub, light_filter_set=light_filter_set,
                                              galprops_data = galprops_data, idx = idx,redshift=redshift,use_scratch=use_scratch)
         if run_type == 'grism': 
             print('\tGenerating broadband.config file for %s...'%run_type)
@@ -351,114 +351,6 @@ def setup_sunrise_illustris_subhalo(snap_cutout,subhalo_object,verbose=True,clob
 
 
 
-
-def setup_sunrise_enzo(snap_fits,prop_file,verbose=True,clobber=True,
-                       stub_dir='$HOME/PythonCode/mock-surveys/stubs_enzo/',
-                       data_dir='$HOME/sunrise_data/',
-                       nthreads=24,redshift_override=None,walltime_limit='02:00:00',use_scratch=True):
-
-    fits_file = os.path.abspath(snap_fits)
-    
-    snap_dir = os.path.dirname(fits_file)
-    
-    print("Setting up sunrise run in.. ", snap_dir)
-
-    stub_dir = os.path.expandvars(stub_dir)
-    data_dir = os.path.expandvars(data_dir)
-    
-    print("Using stubs in.. ",stub_dir)
-    stub_files = np.asarray(glob.glob(os.path.join(stub_dir,'*')))
-
-
-
-    
-    list_of_types = ['images','grism']
-
-    galprops_data = np.load(prop_file)[()]
-    #oh wow
-    dirdir=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(fits_file))))
-    dirdirdir=os.path.join(dirdir,os.path.basename(dirdir))
-    print('checking.. ', dirdir, dirdirdir, fits_file)
-
-    idx = np.argwhere( galprops_data['snap_files']==dirdirdir )[0][0]
-
-    cam_file = fits_file.rstrip('.fits')+'.cameras'
-
-    #real_redshift=gsu.redshift_from_snapshot( subhalo_object['snap'] )
-    #scale_convert=(1.0/(gsu.ilh*(1.0 + real_redshift)))
-
-    #I think this always works in physical kpc anyway?  think further if need other redshifts
-    scale_convert=1.0
-
-    real_redshift=pyfits.open(snap_fits)['YT'].header['REDSHIFT']
-
-    if redshift_override is None:
-        redshift=real_redshift
-    else:
-        redshift=redshift_override
-
-
-    print('redshift= ',redshift)
-    
-    nthreads=str(nthreads)
-        
-    for run_type in list_of_types:
-        run_dir = snap_dir+'/%s'%run_type
-        if not os.path.lexists(run_dir):
-            os.mkdir(run_dir)
-
-        for sf in stub_files:
-            shutil.copy(sf,run_dir)
-            
-        print('\tGenerating sfrhist.config file for %s...'%run_type)
-        sfrhist_fn   = 'sfrhist.config'
-        sfrhist_stub = os.path.join(stub_dir,'sfrhist_base.stub')
-
-        generate_sfrhist_config(run_dir = run_dir, filename = sfrhist_fn, data_dir=data_dir,
-                                stub_name = sfrhist_stub,  fits_file = fits_file, 
-                                galprops_data = galprops_data, run_type = run_type,
-                                nthreads=nthreads, idx = idx,scale_convert=scale_convert,use_scratch=use_scratch,use_cf00=True)
-
-
-        print('\tGenerating mcrx.config file for %s...'%run_type)
-        mcrx_fn   = 'mcrx.config'
-        mcrx_stub = os.path.join(stub_dir,'mcrx_base.stub')
-
-        generate_mcrx_config(run_dir = run_dir, snap_dir = snap_dir, filename = mcrx_fn, 
-                             stub_name = mcrx_stub,npix=1000,
-                             galprops_data = galprops_data, run_type = run_type, nthreads=nthreads, cam_file=cam_file , idx = idx,use_scratch=use_scratch, aux_part_only=True)
-
-
-        
-        if run_type == 'images': 
-            print('\tGenerating broadband.config file for %s...'%run_type)
-            broadband_fn   = 'broadband.config'
-            broadband_stub = os.path.join(stub_dir,'broadband_base.stub')
-
-            generate_broadband_config_images(run_dir = run_dir, snap_dir = snap_dir, data_dir=data_dir, filename = broadband_fn, 
-                                             stub_name = broadband_stub, 
-                                             galprops_data = galprops_data, idx = idx,redshift=redshift,use_scratch=use_scratch)
-        if run_type == 'grism': 
-            print('\tGenerating broadband.config file for %s...'%run_type)
-            broadband_fn   = 'broadband.config'
-            broadband_stub = os.path.join(stub_dir, 'broadband_base.stub')
-
-            generate_broadband_config_grism(run_dir = run_dir, snap_dir = snap_dir, data_dir=data_dir, filename = broadband_fn, 
-                                            stub_name = broadband_stub, 
-                                            galprops_data = galprops_data, idx = idx,redshift=redshift,use_scratch=use_scratch)
-
-
-
-
-
-        print('\tGenerating sunrise.sbatch file for %s...'%run_type)
-        sbatch_fn   = 'sunrise.sbatch'		
-        final_fn = generate_sbatch(run_dir = run_dir, snap_dir = snap_dir, filename = sbatch_fn, 
-                                   galprops_data = galprops_data, run_type = run_type,ncpus=nthreads,walltime=walltime_limit,use_scratch=use_scratch,candelize=True)
-
-        
-    
-    return
 
 
 
@@ -625,8 +517,10 @@ def generate_broadband_config_images(run_dir, snap_dir, data_dir, filename, stub
     bfz.write('output_file                       %s\n'%(int_dir+'/broadbandz.fits'))
     if light_filter_set is False:
         bfz.write('filter_list                       %s\n'%(data_dir+'sunrise_filters/filters_st'))
-    else:
+    elif light_filter_set is True:
         bfz.write('filter_list                       %s\n'%(data_dir+'sunrise_filters/filters_st_light'))
+    else:
+        bfz.write('filter_list                       %s\n'%(data_dir+'sunrise_filters/'+str(light_filter_set)))
         
     bfz.write('filter_file_directory             %s\n'%(data_dir+'sunrise_filters/'))
     bfz.close()
